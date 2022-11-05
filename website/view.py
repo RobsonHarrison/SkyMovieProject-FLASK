3680e27f36828f04e4b2175c963efdf7e1ba0267
Robson Harrison <97robsonharrison@gmail.com>
2022-11-05 10:35:29 +0000

    Initial commit and push of local directory
    Initial publish of the GIT repo.

---
+++ website/view.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def Home():
+    return render_template("home.html")
+
+@views.route('VIP')
+def VIP():
+    return render_template("VIP.html")
