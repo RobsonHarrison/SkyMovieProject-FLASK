4b0ef972b350359da9361c9eafd2404ac269d4c9
Robson Harrison <97robsonharrison@gmail.com>
2022-11-08 09:05:05 +0000

    SQL code added to read all from movies table
    Movie titles and synapsis on homepage is now being read from the SQL table.

--- website/view.py
+++ website/view.py
@@ -1,10 +1,32 @@
 from flask import Blueprint, render_template
 
+#SQL for movie information
+import mysql.connector
+
+db = mysql.connector.connect(
+    user = "root",
+    host = "localhost",
+    password = "",
+    database="skymovie_christmas"
+)
+my_cursor = db.cursor()
+query = ("SELECT film_title FROM movies")
+my_cursor.execute(query)
+for movies in my_cursor:
+    movie_title = tuple(my_cursor.fetchall())
+    # print(type(movie_title))
+my_cursor = db.cursor()
+query = ("SELECT film_synopsis FROM movies")
+my_cursor.execute(query)
+for movies in my_cursor:
+    #print (movies)
+    movie_synopsis=my_cursor.fetchall()
+
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def Home():
-    return render_template("home.html")
+    return render_template("home.html", movietitle=movie_title, moviesynopsis=movie_synopsis)
 
 @views.route('VIP')
 def VIP():
